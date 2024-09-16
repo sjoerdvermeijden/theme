3aaf5803526c7d56f6b01dbbc18c4238c81dd5f9
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-09-16 14:52:58 +0200

    Removes the header menu

--- header.php
+++ header.php
@@ -42,76 +42,6 @@
 <body <?php body_class(); ?>>
 	<div id="page" class="site">
 
-		<div class="site__offcanvas">
-
-			<div class="offcanvas">
-
-				<div class="offcanvas__header">
-
-					<button class="offcanvas__close js-close">
-						<span></span>
-						<span></span>
-						<span></span>
-					</button>
-
-				</div>
-
-				<div class="offcanvas__menu">
-
-					<nav class="mobile-navigation">
-
-						<?php
-						wp_nav_menu(array(
-							'theme_location' 	=> 'menu-1',
-							'container'			=> false,
-							'items_wrap'		=> '<ul class="mobile-navigation__items">%3$s</ul>',
-							'walker'  			=> new Walker_Quickstart_Menu() //use our custom walker
-						));
-						?>
-
-					</nav><!-- #site-navigation -->
-
-					<?php
-					class Walker_Quickstart_Menu extends Walker_Nav_Menu
-					{
-						public function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0)
-						{
-							$classes = array();
-
-							if (!empty($item->classes)) {
-								$classes = (array) $item->classes;
-							}
-
-							$class_names = array();
-
-							if (in_array('current-menu-item', $classes) || in_array('current-menu-parent', $classes) || in_array('current-page-ancestor', $classes)) {
-								$class_names[] = 'mobile-navigation__item--current';
-							}
-
-							if (in_array('menu-item-has-children', $classes)) {
-								$class_names[] = 'has-submenu';
-							}
-
-							$class_names = join(' ', $class_names);
-
-							$class_names = $class_names ? ' ' . esc_attr($class_names) . '' : '';
-
-							$url = '';
-							if (!empty($item->url)) {
-								$url = $item->url;
-							}
-
-							$output .= '<li class="mobile-navigation__item' . $class_names . '"><a href="' . $url . '" class="mobile-navigation__link">' . $item->title . '</a>';
-						}
-					}
-					?>
-
-				</div>
-
-			</div>
-
-		</div>
-
 		<?php if (is_front_page()) : ?>
 
 			<header class="header header--front-page">
@@ -132,64 +62,10 @@
 
 								<?php get_template_part('template-parts/logo'); ?>
 
-								<nav class="navigation">
-									<?php
-									wp_nav_menu(array(
-										'theme_location' 	=> 'menu-1',
-										'container'			=> false,
-										'items_wrap'		=> '<ul class="navigation__items">%3$s</ul>',
-										'walker'  			=> new Primary_Walker_Nav_Menu()
-									));
-									?>
-								</nav><!-- #site-navigation -->
-
-								<?php
-								class Primary_Walker_Nav_Menu extends Walker_Nav_Menu
-								{
-									public function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0)
-									{
-										$classes = array();
-
-										if (!empty($item->classes)) {
-											$classes = (array) $item->classes;
-										}
-
-										$class_names = array();
-
-										if (in_array('current-menu-item', $classes) || in_array('current-menu-parent', $classes) || in_array('current-page-ancestor', $classes)) {
-											$class_names[] = 'navigation__item--current';
-										}
-
-										if (in_array('menu-item-has-children', $classes)) {
-											$class_names[] = 'has-submenu';
-										}
-
-										$class_names = join(' ', $class_names);
-
-										$class_names = $class_names ? ' ' . esc_attr($class_names) . '' : '';
-
-										$url = '';
-										if (!empty($item->url)) {
-											$url = $item->url;
-										}
-
-										$output .= '<li class="navigation__item' . $class_names . '"><a href="' . $url . '" class="navigation__link">' . $item->title . '</a>';
-									}
-								}
-								?>
-
-								<a href="" class="navigation-button js-mobile-button">
-									<span></span>
-									<span></span>
-									<span></span>
-								</a>
-
 							</div>
 
 						</div>
 
 					</div>
-
-				</div>
 
 				</header><!-- #masthead -->
